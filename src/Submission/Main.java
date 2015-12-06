cb502838cb72075206407aaee4cac4548cb79978
Connor Flanigan <connorflanigan@gmail.com>
2015-12-06 14:35:22 -0500

    Fuller driver is implemented

--- src/Submission/Main.java
+++ src/Submission/Main.java
@@ -32,7 +32,32 @@ public class Main {
 	}
 	
 	public static IBuilding createFuller(){
-		// Fill in driver code here
-		return null;
+		Building fuller = new Building();
+		
+		Room room021 = new Room(021, 25, "non-tiered", true, null, ComputerType.WINDOWS);
+		room021 = RoomFactory.getRoom(room021.getType());
+		fuller.addRoom(room021);
+		
+		Room room100 = new Room(100, 102, "tiered", false, DeskType.SWINGOUT, null);
+		room021 = RoomFactory.getRoom(room100.getType());
+		fuller.addRoom(room100);
+		
+		Room room200 = new Room(200, 196, "tiered", false, DeskType.SWINGOUT, null);
+		room200 = RoomFactory.getRoom(room200.getType());
+		fuller.addRoom(room200);
+		
+		Room room222 = new Room(222, 27, "non-tiered", true, null, ComputerType.LINUX);
+		room222 = RoomFactory.getRoom(room222.getType());
+		fuller.addRoom(room222);
+		
+		Room room311 = new Room(311, 38, "non-tiered", true, DeskType.LONGTABLES, null);
+		room021 = RoomFactory.getRoom(room311.getType());
+		fuller.addRoom(room311);
+		
+		Room room320 = new Room(320, 65, "non-tiered", true, DeskType.SWINGOUT, null);
+		room021 = RoomFactory.getRoom(room320.getType());
+		fuller.addRoom(room320);
+		
+		return fuller;
 	}
 }
